3002541960d86573d5b9743d2a2cab101b6b3cfb
m0dulum <tylergriego@outlook.com>
2016-10-04 19:16:04 -0700

    Update mChat.lua

--- mChat.lua
+++ mChat.lua
@@ -2,16 +2,22 @@
 -- @modulum || October 4th, 2016
 
 local remote = game.ReplicatedStorage.remote
+local chat = game:GetService("Chat")
 
 remote.chat.OnServerEvent:connect(function(plr, input)
+	local filtered = chat:FilterStringAsync(input, plr, plr)
+	
 	if plr.UserId == 142762267 then
-		remote.chat:FireAllClients(plr, input, "mod")
+		remote.chat:FireAllClients(plr, filtered, "mod")
+	elseif plr.UserId == 16826035 then
+		remote.chat:FireAllClients(plr, filtered, "yellow")
 	else
-		remote.chat:FireAllClients(plr, input)
+		remote.chat:FireAllClients(plr, filtered)
 	end
 end)
 
 remote.whisper.OnServerEvent:connect(function(plr, musr, str)
+	local filtered = chat:FilterStringAsync(str, plr, plr)
 	remote.whisper:FireClient(plr, musr, str, "send")
 	remote.whisper:FireClient(musr, plr, str, "recieve")
 end)
